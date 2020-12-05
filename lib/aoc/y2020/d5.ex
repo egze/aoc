b77cba450ddb7760093bfa43e9ab6f648f9041b7
Aleksandr Lossenko <29657+egze@users.noreply.github.com>
2020-12-05 13:53:41 +0100

    use binary approach (#8)

--- lib/aoc/y2020/d5.ex
+++ lib/aoc/y2020/d5.ex
@@ -27,33 +27,9 @@ defmodule Aoc.Y2020.D5 do
   end
 
   def get_seat_id(seat) do
-    {row, col, _, _} =
-      seat
-      |> String.graphemes()
-      |> Enum.reduce({0, 0, 0..127, 0..7}, fn char, {row, col, row_range, col_range} ->
-        case char do
-          "F" ->
-            first..last = row_range
-            new_row_last = floor((first + last) / 2)
-            {first, col, first..new_row_last, col_range}
-
-          "B" ->
-            first..last = row_range
-            new_row_first = round((first + last) / 2)
-            {new_row_first, col, new_row_first..last, col_range}
-
-          "R" ->
-            first..last = col_range
-            new_col_first = round((first + last) / 2)
-            {row, new_col_first, row_range, new_col_first..last}
-
-          "L" ->
-            first..last = col_range
-            new_col_last = floor((first + last) / 2)
-            {row, first, row_range, first..new_col_last}
-        end
-      end)
-
-    row * 8 + col
+    seat
+    |> String.graphemes()
+    |> Enum.map(fn char -> if char in ["F", "R"], do: 1, else: 0 end)
+    |> Integer.undigits(2)
   end
 end
