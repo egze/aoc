ecf02a90eefdf1e53569d1162749aad7f755c638
Aleksandr Lossenko <aleksandr.lossenko@gmail.com>
2020-12-07 08:27:05 +0100

    prepare input

---
+++ lib/aoc/y2020/d7.ex
@@ -0,0 +1,13 @@
+defmodule Aoc.Y2020.D7 do
+  use Aoc.Boilerplate,
+    transform: fn raw ->
+      raw
+      |> String.split("\n")
+    end
+
+  def part1(input \\ processed()) do
+  end
+
+  def part2(input \\ processed()) do
+  end
+end
